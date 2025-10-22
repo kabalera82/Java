9aa7af8bec31f8207321b70d62cba8d6277c68f0
Marcos Padilla <kabalera82@gmail.com>
2025-10-22 12:35:36 +0200

    actualiza

---
+++ Tema12AcesoDatos/ejercicio03/src/main/java/LeyendoCsv.java
@@ -0,0 +1,16 @@
+package Tema12AcesoDatos.ejercicio03.src.main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeyendoCsv {
+
+    //Iniciamos la lista
+    List<String> lector = new ArrayList<>();
+
+    //Acumulamos el resultado en
+    StringBuilder st = new StringBuilder();
+
+    //
+
+}
