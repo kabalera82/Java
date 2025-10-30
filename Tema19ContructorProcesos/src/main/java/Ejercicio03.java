95151ee935c9eab212d8281ae905dae63cc6c731
kabalera82 <kabalera@gmail.com>
2025-10-30 22:15:58 +0100

    ghibli API

--- Tema19ContructorProcesos/src/main/java/Ejercicio03.java
+++ Tema19ContructorProcesos/src/main/java/Ejercicio03.java
@@ -6,24 +6,5 @@ import java.io.InputStreamReader;
 
 public class Ejercicio03 {
 
-    public static void main(String[] args) {
-        try {
-            ProcessBuilder pb = new ProcessBuilder("cmd","/c","comando_inexistente");
-            Process p = pb.start();
 
-            BufferedReader err = new BufferedReader(
-                    new InputStreamReader(p.getErrorStream())
-            );
-            String linea;
-            while((linea = err.readLine()) != null){
-                System.out.println("ERROR:"+linea);
-            }
-
-            p.waitFor();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
 }
